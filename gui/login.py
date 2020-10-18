a7be0fdd1567c192f75c09a3e894b9162f155610
hasanaligathamaniya <hasanaligathamaniya476@gmail.com>
2020-10-18 23:23:03 +0530

    login.py updated done change

--- gui/login.py
+++ gui/login.py
@@ -1,15 +1,39 @@
 from tkinter import *  
+import tkinter.font as font
 
 root = Tk()  
 
 root.iconbitmap('D:\collegeproject\gui\image\icon.ico')
 # Defining the window size
-root.geometry("200x150")
+root.geometry("1300x720")
 # Fixing the window size
 root.resizable()
 # Title bar Title
 root.title('cms project')
 # Title Bar Icon
+
+
+#Left side poster design
+#-----------------------------------------------------------------------------------
+myFont = font.Font(family='Helvetica', size=90, weight='bold')
+
+frameLeft = Frame(root,borderwidth=1, relief="groove",width=700,height=802,bg="#12dea7",pady=220)
+frameLeft.pack(side=LEFT)
+frameLeft.pack_propagate(0)
+
+poster = Label(frameLeft, text="Poster", fg="White",bg="#12dea7")
+poster['font']=myFont
+poster.pack()
+#Right Login form design
+#------------------------------------------------------------------------------------
+frameRight = Frame(root,borderwidth=1, relief="groove",width=850,height=802,bg="#fff")
+frameRight.pack(side=RIGHT)
+frameRight.pack_propagate(0)
+
+loginForm = Label(frameRight, text="Red",bg="#fff")
+loginForm.pack()
+
+
 
 
 root.mainloop()  
