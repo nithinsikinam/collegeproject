42d6805f922fef31e2d1f705623718ef143b2ea8
hasanaligathamaniya <hasanaligathamaniya476@gmail.com>
2020-10-18 22:21:50 +0530

    ddd

--- gui/login.py
+++ gui/login.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()  
 
-root.iconbitmap('D:\college project\gui\image\icon.ico')
+root.iconbitmap('D:\collegeproject\gui\image\icon.ico')
 # Defining the window size
 root.geometry("200x150")
 # Fixing the window size
