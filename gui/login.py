0fe35410beffeffd9baecb6ac7ed2d6479c8fff1
nithinsikinam <nithinsikinam@gmail.com>
2020-10-18 20:23:24 +0530

    icon

--- gui/login.py
+++ gui/login.py
@@ -1,7 +1,8 @@
 from tkinter import *  
 
 root = Tk()  
-
+photo = PhotoImage(file = "icon.ico")
+root.iconphoto(False, photo)
 # Defining the window size
 root.geometry("200x150")
 # Fixing the window size
