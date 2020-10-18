885a1934284ac122beb7bca16c11b7e359a5c54e
hasanaligathamaniya <hasanaligathamaniya476@gmail.com>
2020-10-18 20:16:55 +0530

    hello

---
+++ gui/login.py
@@ -0,0 +1,14 @@
+from tkinter import *  
+
+root = Tk()  
+
+# Defining the window size
+root.geometry("200x150")
+# Fixing the window size
+root.resizable()
+# Title bar Title
+root.title('cms project')
+# Title Bar Icon
+root.iconbitmap('icon.ico')
+
+root.mainloop()  
