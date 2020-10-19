f69ce45b9292e25ed48c9b12307ced2e6ff57ea3
hasanaligathamaniya <hasanaligathamaniya476@gmail.com>
2020-10-19 23:06:51 +0530

    update by hasan ali

---
+++ sample code/page.py
@@ -0,0 +1,12 @@
+ argument=0
+ 
+string numbers_to_strings(int argument){ 
+ switch(argument) { 
+        case 0: 
+            return "zero"; 
+        case 1: 
+            return "one"; 
+        case 2: 
+            return "two"; 
+        default: 
+            return "nothing"; 
