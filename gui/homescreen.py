2db0f7f443a54674678db7692af47c0c8a77070a
nithinsikinam <nithinsikinam@gmail.com>
2020-10-19 18:35:35 +0530

    homescreen added

---
+++ gui/homescreen.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root = Tk()  
+
+root.iconbitmap('D:\college project\gui\image\icon.ico')
+# Defining the window size
+root.geometry("1300x720")
+# Fixing the window size
+root.resizable()
+# Title bar Title
+root.title('cms project')
+# Title Bar Icon
+root.mainloop()
