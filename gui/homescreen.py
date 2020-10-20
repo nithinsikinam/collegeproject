3c2b629346297e066f43b22404e1c3edc7198050
nithinsikinam <nithinsikinam@gmail.com>
2020-10-20 14:32:58 +0530

    changes

--- gui/homescreen.py
+++ gui/homescreen.py
@@ -7,29 +7,44 @@ root = Tk()
 
 root.iconbitmap('D:\college project\gui\image\icon.ico')
 # Defining the window size
-root.geometry("1300x720")
+screenw= root.winfo_screenwidth()
+screenh=root.winfo_screenheight()
+root.geometry("%dx%d" %(screenw,screenh) )
 # Fixing the window size
 root.resizable()
 # Title bar Title
 root.title('cms project')
 
+global idw
+global framew
+global frameh
+
+if (screenw==1920):
+    idw=420
+    framew=500
+    frameh=1020
+elif (screenw==1600):
+    idw=400
+    framew=400  
+    frameh=900
+print(framew)
 
 #--------------------------------------------------------------------------------------------------------
 #frames
-frameL = Frame(root,borderwidth=10, relief="groove",width=475,height=650,bg="#133c55")
+frameL = Frame(root,borderwidth=10, relief="groove",width=framew,height=650,bg="#133c55")
 frameL.grid(row=0,column=1)
 frameL.pack_propagate(0)
 
-framem = Frame(root,borderwidth=10, relief="groove",width=475,height=650,bg="#133c55")
+framem = Frame(root,borderwidth=10, relief="groove",width=framew,height=650,bg="#133c55")
 framem.grid(row=0,column=2)
 framem.pack_propagate(0)
 
-framer = Frame(root,borderwidth=10, relief="groove",width=475,height=650,bg="#133c55")
+framer = Frame(root,borderwidth=10, relief="groove",width=framew,height=650,bg="#133c55")
 framer.grid(row=0,column=3)
 framer.pack_propagate(0)
 
 
-frame1 = Frame(root,borderwidth=0, relief="groove",width=500,height=1050,bg="grey")
+frame1 = Frame(root,borderwidth=0, relief="groove",width=idw,height=frameh,bg="grey")
 frame1.grid(row=0,column=0)
 frame1.pack_propagate(0)
 
@@ -58,5 +73,7 @@ img = ImageTk.PhotoImage(Image.open("D:\college project\gui\image\ok.png"))
 panel = Label(frame1, image = img)
 panel.pack()
 
+
+print(str(root.winfo_screenwidth()))
 
 root.mainloop()
