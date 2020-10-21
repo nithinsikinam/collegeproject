f7b7f12f991fd24e96e5b954b22b93dc6d452cea
nithinsikinam <nithinsikinam@gmail.com>
2020-10-21 19:24:49 +0530

    ok

---
+++ gui/nithinplannedhs.py
@@ -0,0 +1,24 @@
+from tkinter import *  
+
+root = Tk()  
+
+root.iconbitmap('D:\collegeproject\gui\image\icon.ico')
+# Defining the window size
+root.geometry("200x150")
+# Fixing the window size
+root.resizable()
+# Title bar Title
+root.title('cms project')
+# Title Bar Icon
+
+class channel:
+    def __init__(self,name,type):
+        self.name= name
+        self.type= type 
+
+    def createicon():
+        =Button(root,Text=se)
+
+
+
+root.mainloop()  
