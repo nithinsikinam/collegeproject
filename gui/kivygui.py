15d06a4395c6858af96e8535dc9f5ae4e93ff573
nithinsikinam <nithinsikinam@gmail.com>
2020-11-13 18:53:34 +0530

    ok smol change

--- gui/kivygui.py
+++ gui/kivygui.py
@@ -1,11 +1,17 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+
+
+class mygridlayout(GridLayout):
+    def __init__(self,**kwargs):
+        
 
 class myapp(App):
-    def build():
+    def build(self):
         return Label(text="hello my friend")
 
 if __name__=="__main__":
-    myapp().build()
+    myapp().run()
 
