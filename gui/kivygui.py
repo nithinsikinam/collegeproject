7822517f232d0087c830ff66eb9f0756deb0a7d9
nithinsikinam <nithinsikinam@gmail.com>
2020-11-13 18:04:44 +0530

    ok

--- gui/kivygui.py
+++ gui/kivygui.py
@@ -2,3 +2,10 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 
+class myapp(App):
+    def build():
+        return Label(text="hello my friend")
+
+if __name__=="__main__":
+    myapp().build()
+
